c3b616204c88548607fdcde5d2f6fa696b604837
Ruchit Patel <rpruchitpatel14@gmail.com>
2025-07-01 12:32:22 +0000

    adding lables to issue

---
+++ force_trans_customization/force_trans_customization/doctype/label/test_label.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Sayaji Infotech and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLabel(FrappeTestCase):
+	pass
